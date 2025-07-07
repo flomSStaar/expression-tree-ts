11c85725c0f0b9ef3fdbe8a7167092a28f6f5e49
Florent MARQUES <f.marques@overscan.fr>
2025-07-07 11:23:21 +0200

    :wrench: update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     include: ['tests/**/*.test.ts'],
     coverage: {
       include: ['src/**/*.ts'],
-      reporter: ['lcov'],
+      reporter: ['text', 'lcov'],
     },
   },
 })
